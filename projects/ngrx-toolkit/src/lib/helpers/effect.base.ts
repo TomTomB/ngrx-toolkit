0f3dc09823350688b4aa02fd6c68e88f29a2ed52
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2021-06-08 23:53:17 +0200

    feat: 🎸 add effect helpers for different flattening operators

--- projects/ngrx-toolkit/src/lib/helpers/effect.base.ts
+++ projects/ngrx-toolkit/src/lib/helpers/effect.base.ts
@@ -1,13 +1,33 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Observable, of } from 'rxjs';
-import { catchError, map, mergeMap } from 'rxjs/operators';
+import {
+  catchError,
+  concatMap,
+  exhaustMap,
+  map,
+  mergeMap,
+  switchMap,
+} from 'rxjs/operators';
 import { TypedActionObject } from '../types/types';
 import { buildErrorFromHttpError } from './error.helpers';
 
 export class EffectBase {
   constructor(private __actions$: Actions, private __featureService: any) {}
 
+  /**
+   * @deprecated use `onActionMergeMap` or `onActionSwitchMap`
+   */
   onAction({
+    action,
+    serviceCall,
+  }: {
+    action: TypedActionObject;
+    serviceCall: (args: any) => Observable<any>;
+  }) {
+    return this.onActionMergeMap({ action, serviceCall });
+  }
+
+  onActionSwitchMap({
     action,
     serviceCall,
   }: {
@@ -17,17 +37,68 @@ export class EffectBase {
     return createEffect(() =>
       this.__actions$.pipe(
         ofType(action.call),
-        mergeMap(({ args }) =>
-          serviceCall
-            .bind(this.__featureService)(args)
-            .pipe(
-              map((response) => action.success({ response, args })),
-              catchError((error) =>
-                of(action.failure(buildErrorFromHttpError({ error, args })))
-              )
-            )
-        )
+        switchMap(({ args }) => this._serviceCall(action, args, serviceCall))
       )
     );
+  }
+
+  onActionMergeMap({
+    action,
+    serviceCall,
+  }: {
+    action: TypedActionObject;
+    serviceCall: (args: any) => Observable<any>;
+  }) {
+    return createEffect(() =>
+      this.__actions$.pipe(
+        ofType(action.call),
+        mergeMap(({ args }) => this._serviceCall(action, args, serviceCall))
+      )
+    );
+  }
+
+  onActionExhaustMap({
+    action,
+    serviceCall,
+  }: {
+    action: TypedActionObject;
+    serviceCall: (args: any) => Observable<any>;
+  }) {
+    return createEffect(() =>
+      this.__actions$.pipe(
+        ofType(action.call),
+        exhaustMap(({ args }) => this._serviceCall(action, args, serviceCall))
+      )
+    );
+  }
+
+  onActionConcatMap({
+    action,
+    serviceCall,
+  }: {
+    action: TypedActionObject;
+    serviceCall: (args: any) => Observable<any>;
+  }) {
+    return createEffect(() =>
+      this.__actions$.pipe(
+        ofType(action.call),
+        concatMap(({ args }) => this._serviceCall(action, args, serviceCall))
+      )
+    );
+  }
+
+  private _serviceCall(
+    action: TypedActionObject,
+    args: any,
+    serviceCall: (args: any) => Observable<any>
+  ) {
+    return serviceCall
+      .bind(this.__featureService)(args)
+      .pipe(
+        map((response) => action.success({ response, args })),
+        catchError((error) =>
+          of(action.failure(buildErrorFromHttpError({ error, args })))
+        )
+      );
   }
 }
